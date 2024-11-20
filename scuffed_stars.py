221c4ed3c08d11e66fc2962ba15c1f84774290b9
Tat2feuille <tat2feuille@gmail.com>
2024-11-20 21:01:21 +0100

    Update scuffed_stars.py
    i think im done with walls water and bushes already?

--- scuffed_stars.py
+++ scuffed_stars.py
@@ -1,5 +1,6 @@
 #CONFIGURATION
 import kandinsky as k
+
 class shape:
     def __init__(self,x,y,color):
         self.x=x
@@ -16,25 +17,29 @@ class shape:
             colorNB[(1-i)%3]+=colorS[i]
         colorN=[int(e.zfill(8)[::-1],2) for e in colorNB]
         self.color=tuple(colorN)
+
 class rectangle(shape):
     def __init__(self,x,y,width,height,color):
         super().__init__(self,x,y,color)
         self.height=height
         self.width=width
     def render(self):
-        k.fill_rect(self.x-camX,self.y-camY,self.height,self.width,(0,0,0))
+        k.fill_rect(round(self.x-camX-self.width//2),round(self.y-camY-self.height//2),self.height,self.width,(0,0,0))
+
 class circle(shape):
     def __init__(self,x,y,radius,color):
         super().__init__(self,x,y,color)
         self.radius=radius
     def render(self):
-        k.fill_circle(self.x-camX,self.y-camY,self.height,self.width,(0,0,0))
+        k.fill_circle(round(self.x-camX),round(self.y-camY),self.height,self.width,(0,0,0))
+
 class wall(rectangle):
     def __init__(self,x,y,width,height,color,toughness):
         super().__init__(x,y,width,height,color)
         super().putdata(0,3,0)
         self.toughness=toughness
         super().putdata(3,2,toughness)
+
 class water(rectangle):
     def __init__(self,x,y,width,height,color,hover,breakable):
         super().__init__(x,y,width,height,color)
@@ -43,14 +48,16 @@ class water(rectangle):
         self.breakable=breakable
         super().putdata(3,1,hover)
         super().putdata(4,1,breakable)
+
 class bush(rectangle):
     def __init__(self,x,y,width,height,color):
         super().__init__(x,y,width,height,color)
         super().putata(0,3,2)
+
 class projectile(circle):
     def __init__(self,x,y,radius,pierce,wallbreak,damage,team):
         super().__init__(x,y,radius,color)
-        super().putdata(0,3,0)
+        super().putdata(0,3,3)
         self.pierce=pierce
         self.wallbreak=wallbreak
         self.damage=damage
@@ -63,22 +70,27 @@ class projectile(circle):
         self.direction=0
         self.velocity=0
         self.range=0
+
 class player(circle):
     def __init__(self,x,y,team):
         super().__init__(x,y,10,color)
         super().putdata(0,3,0)
         super().putdata(3,3,team)
         self.health=100
+
 class shelly(player):
     def __init__(self,x,y,team):
         super().__init__(self,x,y,width,radius,team)
         self.health=37
     def attack(self,di):
         return [shellyAttack(x,y,di+i,team) for i in range(-15,15+1,7.5)]
+
 class shellyAttack(projectile):
     def __init__(self,x,y,di,team):
         super().__init__(self,x,y,10,0,0,3,team)
         self.velocity=3100
+        self.life=100
+
 def getdata(c,k,l):
     colorS=""
     colorB=[str(bin(e))[2:].zfill(8) for e in c]
@@ -86,17 +98,82 @@ def getdata(c,k,l):
         colorS=colorB[(1+i)%3][i//3]+colorS
     colorV=colorS[k:k+l]
     return int(colorV[::-1],2)
+
 def refreshScreen(color=(255,255,255)):
     k.fill_rect(0,0,320,222,color)
+
 def processEntities(entities):
-    wallsTick
+    bushesRender(entities['bushes'])
+    waterRender(entities['water'])
+    wallsRender(entities['walls'])
+    playersRender(entities['players'])
+    projectilesRender(entities['projectiles'])
+    bushesTick(entities['bushes'])
+    waterTick(entities['water'])
+    wallsTick(entities['walls'])
+    playersTick(entities['players'])
     projectilesTick(entities['projectiles'])
-def projectilesTick(pr):
+
+def bushesRender(bs):
+    for e in bs:
+        e.render()
+
+def waterRender(wt):
+    for e in wt:
+        e.render()
+
+def wallsRender(wl):
+    for e in wl:
+        e.render()
+
+def playersRender(pl):
+    for e in pl:
+        e.render()
+
+def projectilesRender(pr):
+    for e in pr:
+        e.render()
+
+def bushesTick(bs):
+    for e in bs:
+        c=k.get_pixel(e.x-camX,e.y-camY)
+        if getdata(c,0,3)==3:
+            if getdata(4,1)==1:
+                del e
+
+def waterTick(wt):
+    for e in wt:
+        c=k.get_pixel(e.x-camX,e.y-camY)
+        if getdata(c,0,3)==3:
+            if getdata(4,1)==1:
+                if getdata(3,1)==0:
+                    e.render()
+                del e
+
+def wallsTick(wl):
+    for e in wt:
+        c=k.get_pixel(e.x-camX,e.y-camY)
+        if getdata(c,0,3)==3:
+            if getdata(4,1)==1:
+                if getdata(3,1)==0:
+                    e.render()
+                del e
+
+def playersTick(pl):
     pass
+
+def projectilesTick(pr):
+    for e in wt:
+        c=k.get_pixel(e.x-camX,e.y-camY)
+        if getdata(c,0,3)!=3:
+            e.render()
+            del e
+
 #GAME SETUP
 camX=0
 camY=0
 Entities={'walls':[],
+          'players':[],
           'water':[],
           'bushes':[],
           'projectiles':[]

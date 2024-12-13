25093b8567832014bb4e0a10e72dd1ebcf249054
Tat2feuille <tat2feuille@gmail.com>
2024-12-13 14:32:55 +0100

    fix super.__init__() syntax and entity deletion

--- scuffed_stars.py
+++ scuffed_stars.py
@@ -1,5 +1,10 @@
+#EMULATOR CONFIG
+import os
+os.environ['KANDINSKY_OS_MODE'] = '3'
+
 #CONFIGURATION
 import kandinsky as k
+import math
 
 class shape:
     def __init__(self,x,y,color):
@@ -20,18 +25,18 @@ class shape:
 
 class rectangle(shape):
     def __init__(self,x,y,width,height,color):
-        super().__init__(self,x,y,color)
+        super().__init__(x,y,color)
         self.height=height
         self.width=width
     def render(self):
-        k.fill_rect(round(self.x-camX-self.width//2),round(self.y-camY-self.height//2),self.height,self.width,(0,0,0))
+        k.fill_rect(round(self.x-camX-self.width//2),round(self.y-camY-self.height//2),self.height,self.width,self.color)
 
 class circle(shape):
     def __init__(self,x,y,radius,color):
-        super().__init__(self,x,y,color)
+        super().__init__(x,y,color)
         self.radius=radius
     def render(self):
-        k.fill_circle(round(self.x-camX),round(self.y-camY),self.height,self.width,(0,0,0))
+        k.fill_circle(round(self.x-camX),round(self.y-camY),self.radius,self.color)
 
 class wall(rectangle):
     def __init__(self,x,y,width,height,color,toughness):
@@ -56,7 +61,7 @@ class bush(rectangle):
 
 class projectile(circle):
     def __init__(self,x,y,radius,pierce,wallbreak,damage,team):
-        super().__init__(x,y,radius,color)
+        super().__init__(x,y,radius,(0,0,0))
         super().putdata(0,3,3)
         self.pierce=pierce
         self.wallbreak=wallbreak
@@ -73,6 +78,7 @@ class projectile(circle):
 
 class player(circle):
     def __init__(self,x,y,team):
+        color=(0,0,0)
         super().__init__(x,y,10,color)
         super().putdata(0,3,0)
         super().putdata(3,3,team)
@@ -80,14 +86,14 @@ class player(circle):
 
 class shelly(player):
     def __init__(self,x,y,team):
-        super().__init__(self,x,y,width,radius,team)
+        super().__init__(x,y,width,radius,team)
         self.health=37
     def attack(self,di):
         return [shellyAttack(x,y,di+i,team) for i in range(-15,15+1,7.5)]
 
 class shellyAttack(projectile):
     def __init__(self,x,y,di,team):
-        super().__init__(self,x,y,10,0,0,3,team)
+        super().__init__(x,y,10,0,0,3,team)
         self.velocity=3100
         self.life=100
 
@@ -128,47 +134,62 @@ def wallsRender(wl):
 
 def playersRender(pl):
     for e in pl:
-        e.render()
+        if e.health>0:
+            e.render()
 
 def projectilesRender(pr):
     for e in pr:
         e.render()
 
 def bushesTick(bs):
-    for e in bs:
+    for i in range(len(bs)):
+        e=bs[i]
         c=k.get_pixel(e.x-camX,e.y-camY)
         if getdata(c,0,3)==3:
             if getdata(c,4,1)==1:
-                del e
+                del bs[i]
 
 def waterTick(wt):
-    for e in wt:
+    for i in range(len(wt)):
+        e=wt[i]
         c=k.get_pixel(e.x-camX,e.y-camY)
         if getdata(c,0,3)==3:
             if getdata(c,4,1)==1:
                 if getdata(c,3,1)==0:
                     e.render()
-                del e
+                del wt[i]
 
 def wallsTick(wl):
-    for e in wt:
+    for i in range(len(wl)):
+        e=wl[i]
         c=k.get_pixel(e.x-camX,e.y-camY)
         if getdata(c,0,3)==3:
             if getdata(c,4,1)==1:
                 if getdata(c,3,1)==0:
                     e.render()
-                del e
+                del wl[i]
 
 def playersTick(pl):
-    pass
-
-def projectilesTick(pr):
-    for e in wt:
+    for i in range(len(pl)):
+        e=pl[i]
         c=k.get_pixel(e.x-camX,e.y-camY)
+        if getdata(c,0,3)==3:
+            damage=getdata(c,5,6)
+            e.health-=damage
+            
+            
+def projectilesTick(pr):
+    for i in range(len(pr)):
+        e=pr[i]
+        c=k.get_pixel(round(e.x-camX),round(e.y-camY))
         if getdata(c,0,3)!=3:
             e.render()
-            del e
-
+            del pr[i]
+        else:
+            angle=math.radians(e.direction)
+            e.x+=math.cos(angle)*e.velocity
+            e.y+=math.sin(angle)*e.velocity
+            
 #GAME SETUP
 camX=0
 camY=0
@@ -180,6 +201,10 @@ Entities={'walls':[],
           }
 
 #GAME LOOP
+#Entities['players'].append(player(10,10,0))
+Entities['projectiles'].append(projectile(10,10,10,1,1,50,0))
+Entities['projectiles'][0].velocity=1
+Entities['projectiles'][0].direction=15
 while True:
     refreshScreen()
     processEntities(Entities)
